b852efd79f7689a04f717bfe3b96aa9d3d097593
Eliran8hadad <eliran_ha@mba.co.il>
2024-12-10 11:33:56 +0200

    Delete Systems/AccountSystem directory

--- Systems/AccountSystem/WebServices/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Maba.AccountSystem.WebServices.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
