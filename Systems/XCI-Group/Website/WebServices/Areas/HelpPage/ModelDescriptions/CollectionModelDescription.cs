3ba366afec6fb6c508d6aaea5b1cc3e0f7b2eec1
Eliran8hadad <eliran_ha@mba.co.il>
2024-12-10 11:36:09 +0200

    Delete Systems/XCI-Group directory

--- Systems/XCI-Group/Website/WebServices/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Maba.Hydra2.Systems.XCIGroup.WebServices.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Systems/XCI-Group/Website/WebServices/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Maba.Hydra2.Systems.XCIGroup.WebServices.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
