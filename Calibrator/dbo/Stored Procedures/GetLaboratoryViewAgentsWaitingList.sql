ba3ba53b6c48b41785250a35961e7e3813421bf5
edtc <ed.kudlaev@gmail.com>
2025-03-24 15:56:16 +0200

    Add GetLaboratoryViewAgentsWaitingList

--- Calibrator/dbo/Stored Procedures/GetLaboratoryViewAgentsWaitingList.sql
+++ Calibrator/dbo/Stored Procedures/GetLaboratoryViewAgentsWaitingList.sql
@@ -1,21 +1,24 @@
-﻿-- ==================================================
+﻿
+-- ==================================================
 -- Author:		Shamailov Slavik
 -- Create date: 17/02/2025
 -- Description:	Get Work waiting list for maba agents
 -- ==================================================
-CREATE PROCEDURE [GetLaboratoryViewAgentsWaitingList]
+CREATE   PROCEDURE [dbo].[GetLaboratoryViewAgentsWaitingList]
 AS
 BEGIN
 
 	SET NOCOUNT ON;
 
-	SELECT  distinct   servcalls_view.[agent name],servcalls_view.[Customer Number], servcalls_view.[Customer Name], servcalls_view.klita
-	FROM            [31.154.20.231].amaba.dbo.servcalls_view INNER JOIN
-							 [31.154.20.231].amaba.dbo.DOCSTATS ON servcalls_view.[סטטוס תעודת קליטה] = DOCSTATS.STATDES
-	WHERE        (DOCSTATS.DOCSTAT = 62) OR
-							 (DOCSTATS.DOCSTAT = 63) OR
-							 (DOCSTATS.DOCSTAT = 91) OR
-							 (DOCSTATS.DOCSTAT = 58)
-	ORDER BY servcalls_view.[agent name], servcalls_view.klita
+	SELECT DISTINCT sv.[agent name] AS AgentName
+		,sv.[Customer Number] AS CustomerNumber
+		,sv.[Customer Name] AS CustomerName
+		,sv.klita as Klita
+	FROM [31.154.20.231].amaba.dbo.servcalls_view as sv
+	INNER JOIN [31.154.20.231].amaba.dbo.DOCSTATS ON sv.[סטטוס תעודת קליטה] = DOCSTATS.STATDES
+	WHERE DOCSTATS.DOCSTAT IN( 62,63,91,58)
+	ORDER BY sv.[agent name]
+		,sv.klita
+
 
 END
