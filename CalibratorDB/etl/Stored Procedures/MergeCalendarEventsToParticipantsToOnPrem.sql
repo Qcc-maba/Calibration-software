653cd30b9e9fcf90076d4f09b2fd8827b52adc63
edtc <ed.kudlaev@gmail.com>
2025-04-19 15:31:22 +0300

    Add load CalendarEventsToParticipants

---
+++ CalibratorDB/etl/Stored Procedures/MergeCalendarEventsToParticipantsToOnPrem.sql
@@ -0,0 +1,39 @@
+﻿CREATE PROCEDURE etl.MergeCalendarEventsToParticipantsToOnPrem
+AS
+MERGE INTO [dbo].[CalendarEventsToParticipants] AS dest
+USING (
+	SELECT [CalendarEventId]
+		,[UserId]
+		,[CreatedDate]
+		,[UpdatedDate]
+		,[IsDeleted]
+		,[UpdateUserID]
+	FROM [etl].[CalendarEventsToParticipants]
+	) AS source
+	ON dest.[CalendarEventId] = source.[CalendarEventId] AND
+	   dest.[UserId] = source.[UserId] AND
+	   dest.[CreatedDate] = source.[CreatedDate] 
+WHEN MATCHED
+	THEN
+		UPDATE
+		SET  dest.[UpdatedDate] = source.[UpdatedDate]
+			,dest.[IsDeleted] = source.[IsDeleted]
+			,dest.[UpdateUserID] = source.[UpdateUserID]
+WHEN NOT MATCHED BY TARGET
+	THEN
+		INSERT (
+			[CalendarEventId]
+			,[UserId]
+			,[CreatedDate]
+			,[UpdatedDate]
+			,[IsDeleted]
+			,[UpdateUserID]
+			)
+		VALUES (
+			source.[CalendarEventId]
+			,source.[UserId]
+			,source.[CreatedDate]
+			,source.[UpdatedDate]
+			,source.[IsDeleted]
+			,source.[UpdateUserID]
+			);
---
+++ CalibratorDB/etl/Tables/CalendarEventsToParticipants.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [etl].[CalendarEventsToParticipants] (
+    [CalendarEventId] INT           NOT NULL,
+    [UserId]          INT           NOT NULL,
+    [CreatedDate]     DATETIME2 (0) NOT NULL,
+    [UpdatedDate]     DATETIME2 (0) NULL,
+    [IsDeleted]       BIT           NOT NULL,
+    [UpdateUserID]    INT           NULL
+);
+
