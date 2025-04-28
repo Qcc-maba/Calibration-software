5cc5b7427ffd380311ed6cd950307cea6d5888d3
edtc <ed.kudlaev@gmail.com>
2025-04-28 11:16:38 +0300

    add OrderItemsStatuses

---
+++ CalibratorDB/dbo/Stored Procedures/AssignOrderItemsStatus.sql
@@ -0,0 +1,71 @@
+﻿-- =============================================
+-- Author:		Eduard Kudlaiev
+-- Create date: 28/04/2025
+-- Description:	Assign availability for calibrator
+-- JiraLink: 
+-- =============================================
+CREATE   PROCEDURE [dbo].[AssignOrderItemsStatus]
+@UserEmail nvarchar(50),
+@MbaReportNumber nvarchar(20),
+@OrderItemStatusId int,
+@OrderItemsStatusDate datetime2(0)
+/*
+DECLARE @dt DATETIME2(0) = GETDATE()
+
+EXEC [dbo].[AssignOrderItemsStatus]
+@UserEmail ='sinova_super_admin@gmail.com',
+@MbaReportNumber ='2523195/4',
+@OrderItemStatusId = 81,
+@OrderItemsStatusDate = @dt;
+*/
+AS
+BEGIN
+SET NOCOUNT ON;
+
+DECLARE @UserId INT = (SELECT ID FROM [dbo].[Users] WHERE Email = @UserEmail) 
+
+IF NOT EXISTS (
+SELECT 1 FROM [dbo].[Users] as u
+JOIN [dbo].[UsersToUserRoles] as utr ON u.ID = utr.UserId
+JOIN [dbo].[UserRoles] as ur ON ur.UserRoleId = utr.UserRoleId
+WHERE u.ID = @UserId /*AND ur.UserRoleName = 'Calibrator'*/ and u.IsActive = 1
+)
+THROW 51000, 'Provided user is not active', 1;
+
+IF NOT EXISTS (
+SELECT 1 FROM [dbo].[OrderDetails] as od WHERE od.MbaReportNumber = @MbaReportNumber
+)
+THROW 51000, 'Provided MbaReportNumber not exists', 1;
+
+IF NOT EXISTS (
+SELECT 1
+  FROM [dbo].[Statuses] as s
+  JOIN [dbo].[StatusesCategories] as sc ON s.StatusCategoryId = sc.StatusCategoryId
+  WHERE s.StatusId = @OrderItemStatusId AND sc.StatusDescriptionENG = 'ReportStatus'
+)
+THROW 51000, 'Incorrect ReportStatus provided', 1;
+
+IF EXISTS (
+SELECT 1
+  FROM [dbo].[OrderItemsStatuses] as ois
+  WHERE ois.[MbaReportNumber] = @MbaReportNumber AND 
+		ois.[OrderItemStatusDate] = @OrderItemsStatusDate
+)
+THROW 51000, 'Entry already exists. Please check status date', 1;
+
+
+INSERT [dbo].[OrderItemsStatuses]
+           ([MbaReportNumber]
+           ,[OrderItemStatusId]
+           ,[OrderItemStatusDate]
+           ,[UserId]
+           )
+	VALUES
+			(
+			@MbaReportNumber,
+			@OrderItemStatusId,
+			@OrderItemsStatusDate,
+			@UserId 
+			)
+
+END
---
+++ CalibratorDB/dbo/Tables/OrderItemsStatuses.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[OrderItemsStatuses] (
+    [MbaReportNumber]     NVARCHAR (20) NOT NULL,
+    [OrderItemStatusId]   INT           NOT NULL,
+    [OrderItemStatusDate] DATETIME2 (0) NOT NULL,
+    [UserId]              INT           NOT NULL,
+    [CreatedDate]         DATETIME2 (0) DEFAULT (getdate()) NOT NULL,
+    [IsDeleted]           BIT           DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_OrderItemsStatuses] PRIMARY KEY CLUSTERED ([MbaReportNumber] ASC, [OrderItemStatusDate] ASC),
+    CONSTRAINT [FK_OrderItemsStatuses_OrderItemsStatusId] FOREIGN KEY ([OrderItemStatusId]) REFERENCES [dbo].[Statuses] ([StatusId]),
+    CONSTRAINT [FK_OrderItemsStatuses_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([ID])
+);
+
