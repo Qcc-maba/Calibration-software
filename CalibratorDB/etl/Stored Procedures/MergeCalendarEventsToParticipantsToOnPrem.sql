02f034f7600fe88636140e75cf27abef9ac38dc0
edtc <ed.kudlaev@gmail.com>
2025-05-04 11:30:49 +0300

    Fix MergeCalendarEventsToParticipantsToOnPrem

--- CalibratorDB/etl/Stored Procedures/MergeCalendarEventsToParticipantsToOnPrem.sql
+++ CalibratorDB/etl/Stored Procedures/MergeCalendarEventsToParticipantsToOnPrem.sql
@@ -1,14 +1,18 @@
-﻿CREATE PROCEDURE etl.MergeCalendarEventsToParticipantsToOnPrem
+﻿CREATE PROCEDURE [etl].[MergeCalendarEventsToParticipantsToOnPrem]
 AS
 MERGE INTO [dbo].[CalendarEventsToParticipants] AS dest
 USING (
-	SELECT [CalendarEventId]
-		,[UserId]
-		,[CreatedDate]
-		,[UpdatedDate]
-		,[IsDeleted]
-		,[UpdateUserID]
-	FROM [etl].[CalendarEventsToParticipants]
+	SELECT 
+	     ce.[CalendarEventId]
+		,u1.ID as [UserId]
+		,ctp.[CreatedDate]
+		,ctp.[UpdatedDate]
+		,ctp.[IsDeleted]
+		,u2.ID as [UpdateUserID]
+	FROM [etl].[CalendarEventsToParticipants] as ctp
+	JOIN [dbo].[CalendarEvents] as ce ON ce.AWSCalendarEventId = ctp.[CalendarEventId]
+	LEFT JOIN [dbo].[Users] as u1 ON u1.AWSID = ctp.[UserId]
+	LEFT JOIN [dbo].[Users] as u2 ON u2.AWSID = ctp.[UpdateUserID]
 	) AS source
 	ON dest.[CalendarEventId] = source.[CalendarEventId] AND
 	   dest.[UserId] = source.[UserId] AND
