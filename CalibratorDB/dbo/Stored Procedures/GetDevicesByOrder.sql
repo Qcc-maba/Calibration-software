e6b48e296e171f1c7f0b20c1a3ff0addbe2c5e92
edtc <ed.kudlaev@gmail.com>
2025-05-15 11:44:56 +0300

    Apply changes to GetDevicesByOrder

--- CalibratorDB/dbo/Stored Procedures/GetDevicesByOrder.sql
+++ CalibratorDB/dbo/Stored Procedures/GetDevicesByOrder.sql
@@ -4,15 +4,49 @@
 -- Description:	Get devices by order number
 -- =============================================
 CREATE PROCEDURE [dbo].[GetDevicesByOrder] --'LA25100040  '
-	@OrderNumber NVARCHAR(20),
-	@MainCategories NVARCHAR(50) = NULL,
-	@SecondaryCategories NVARCHAR(50) = NULL,
-	@DeviceManufacturer NVARCHAR(255) = NULL,
-	@DeviceModels NVARCHAR(30) = NULL
+	@OrderNumber nvarchar(20),
+	@MainCategories nvarchar(MAX) = NULL,
+	@SecondaryCategories nvarchar(MAX) = NULL,
+	@DeviceManufacturer nvarchar(MAX) = NULL,
+	@DeviceModels nvarchar(MAX) = NULL
 AS
 BEGIN
 
 	SET NOCOUNT ON;
+	
+
+DROP TABLE IF EXISTS #MainCategories
+CREATE TABLE #MainCategories
+(
+MainCategory NVARCHAR(50)
+)
+INSERT #MainCategories(MainCategory)
+SELECT DISTINCT v.Value FROM dbo.ParseCSVToTable(@MainCategories) as v
+
+
+DROP TABLE IF EXISTS #SecondaryCategories
+CREATE TABLE #SecondaryCategories
+(
+SecondaryCategory NVARCHAR(50)
+)
+INSERT #SecondaryCategories(SecondaryCategory)
+SELECT DISTINCT v.Value FROM dbo.ParseCSVToTable(@SecondaryCategories) as v
+
+DROP TABLE IF EXISTS #DeviceManufacturer
+CREATE TABLE #DeviceManufacturer
+(
+DeviceManufacturer NVARCHAR(255)
+)
+INSERT #DeviceManufacturer(DeviceManufacturer)
+SELECT DISTINCT v.Value FROM dbo.ParseCSVToTable(@DeviceManufacturer) as v
+
+DROP TABLE IF EXISTS #DeviceModels
+CREATE TABLE #DeviceModels
+(
+DeviceModel NVARCHAR(30)
+)
+INSERT #DeviceModels(DeviceModel)
+SELECT DISTINCT v.Value FROM dbo.ParseCSVToTable(@DeviceModels) as v
 
 DECLARE @sql NVARCHAR(MAX) =
 CONCAT(
@@ -30,13 +64,13 @@ CONCAT(
 	,od.DeviceManufacturer
 FROM [dbo].[OrderDetails] as od
 JOIN [dbo].[OrderWorkPlans] as op ON od.OrderWorkPlanId = op.OrderWorkPlanId
-WHERE OrderNumber = TRIM(''',@OrderNumber,''')
 '
-,CASE WHEN @MainCategories IS NOT NULL THEN' AND od.MainCategory = '''+@MainCategories+''' 'ELSE ' ' END
-,CASE WHEN @SecondaryCategories IS NOT NULL THEN' AND od.SecondCategory = '''+@SecondaryCategories+''' 'ELSE ' ' END
-,CASE WHEN @DeviceManufacturer IS NOT NULL THEN' AND od.DeviceManufacturer = '''+@DeviceManufacturer+''' 'ELSE ' ' END
-,CASE WHEN @DeviceModels IS NOT NULL THEN' AND od.DeviceModel = '''+@DeviceModels+''' 'ELSE ' ' END
+,IIF(@MainCategories IS NOT NULL,' JOIN #MainCategories as mc ON od.MainCategory COLLATE DATABASE_DEFAULT  = mc.MainCategory COLLATE DATABASE_DEFAULT ',' ')
+,IIF(@SecondaryCategories IS NOT NULL,' JOIN #SecondaryCategories as sc ON od.SecondCategory COLLATE DATABASE_DEFAULT   = sc.SecondaryCategory COLLATE DATABASE_DEFAULT ',' ')
+,IIF(@DeviceManufacturer IS NOT NULL,' JOIN #DeviceManufacturer as dmf ON od.DeviceManufacturer COLLATE DATABASE_DEFAULT  = dmf.DeviceManufacturer COLLATE DATABASE_DEFAULT ',' ')
+,IIF(@DeviceModels IS NOT NULL,' JOIN #DeviceModels as dm ON od.DeviceModel COLLATE DATABASE_DEFAULT = dm.DeviceModel COLLATE DATABASE_DEFAULT ',' ')
 ,'
+WHERE OrderNumber = TRIM(''',@OrderNumber,''')
 ORDER BY OrderNumber
 	,MbaReportNumber
 ')
