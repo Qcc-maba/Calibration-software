4cc14b2ddcb189c4afd1ae6469d9de23b57a56c4
edtc <ed.kudlaev@gmail.com>
2025-06-03 17:27:55 +0300

    Add get all calibrator devices

---
+++ CalibratorDB/dbo/Stored Procedures/GetAllCalibrationDevices.sql
@@ -0,0 +1,55 @@
+﻿CREATE   PROCEDURE [dbo].[GetAllCalibrationDevices]
+@MeasurementDevicesMainClassId INT = NULL,
+@CalibrationDeviceId INT = NULL,
+@ApplyFilterByDevicesParents BIT = 0
+AS
+-- =============================================
+-- Author:		Eduard Kudlaiev
+-- Create date: 28/05/2025
+-- Description:	
+-- JiraLink: https://calibration-maba.atlassian.net/browse/MABA-43
+-- =============================================
+BEGIN
+
+IF @ApplyFilterByDevicesParents = 1 AND @CalibrationDeviceId IS NULL
+THROW 51000, 'Provide @CalibrationDeviceId to get child devices.', 1;
+
+	DECLARE @sql NVARCHAR(MAX) =
+	CONCAT(
+	'
+	SELECT md.[ID]
+		  ,md.[MabaID]
+		  ,md.[Model]
+		  ,md.[SerialNumber]
+		  ,md.[CalibrationDate] AS [LastCalibrationDate]
+		  ,md.[NextCalibration]
+		  ,NULL AS [CommunicationType] 
+		  ,NULL AS [CommunicationDetails]
+		  ,NULL AS [Rate]
+		  ,NULL AS [Intervals] 
+		  ,NULL AS [Status]
+		  ,mc.NameHebrew
+		  ,u.ShortNameHe AS UnitName
+		  ,md.WorkRangeMin	as LowerDomainBorder
+		  ,md.WorkRangeMax as UpperDomainBorder 
+		  ,m.NameHe as MeasurmentName
+		  ,d.DepartmentName
+		  ,''not defined yet'' as Channels
+	  FROM [dbo].[MeasurementDevices] as md
+	  '
+	  ,CASE WHEN @ApplyFilterByDevicesParents = 1 THEN ' JOIN [dbo].[MeasurementDevicesParents] as pf ON md.[ID] = pf.[DeviceId] AND pf.[ParentId] ='+CAST(@CalibrationDeviceId as NVARCHAR(50))+'' ELSE ' ' END
+	  ,'
+	  JOIN [dbo].[MeasurementDevicesMainClasses] as mc ON md.MainClass = mc.Id
+	  JOIN [dbo].[Units] as u ON md.Unit = u.ID
+	  JOIN [dbo].[Measurements] as m ON md.MeasurementId = m.ID
+	  JOIN [dbo].[Departments] as d ON md.DepartmentId = d.ID
+	  WHERE md.RemoveDate IS NULL AND md.IsDeleted = 0
+	  '
+	  ,CASE WHEN @MeasurementDevicesMainClassId IS NOT NULL AND @ApplyFilterByDevicesParents = 0 THEN' AND md.MainClass = '+ +CAST(@MeasurementDevicesMainClassId as NVARCHAR(50))+' 'ELSE ' ' END
+	  ,CASE WHEN @CalibrationDeviceId IS NOT NULL AND @ApplyFilterByDevicesParents = 0 THEN' AND md.[ID] = '+ +CAST(@CalibrationDeviceId as NVARCHAR(50))+' 'ELSE ' ' END 
+	  )
+
+	PRINT @sql
+	EXEC sp_executesql @sql
+
+END
---
+++ CalibratorDB/dbo/Stored Procedures/GetAllMeasurementDevicesMainClasses.sql
@@ -0,0 +1,14 @@
+﻿CREATE   PROCEDURE [dbo].[GetAllMeasurementDevicesMainClasses]
+AS
+-- =============================================
+-- Author:		Eduard Kudlaiev
+-- Create date: 03/0/2025
+-- Description:	
+-- JiraLink: https://calibration-maba.atlassian.net/browse/MABA-43
+-- =============================================
+
+SELECT 
+	Id, 
+	NameHebrew,
+	NameEnglish
+FROM [dbo].[MeasurementDevicesMainClasses]
