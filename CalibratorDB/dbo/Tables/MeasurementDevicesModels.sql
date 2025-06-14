13b65781f51b65a0eb2edf8f8b8e5678a2e1b728
edtc <ed.kudlaev@gmail.com>
2025-06-14 15:53:01 +0300

    Fix bug in ETL found during testing

--- CalibratorDB/dbo/Tables/MeasurementDevicesModels.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[MeasurementDevicesModels] (
-    [ID]          INT           IDENTITY (1, 1) NOT NULL,
-    [Model]       NVARCHAR (50) NOT NULL,
-    [Description] NVARCHAR (50) NULL,
-    [DateAdd]     DATETIME      CONSTRAINT [DF_MeasurementDevicesModels_DateAdd] DEFAULT (getdate()) NOT NULL,
-    CONSTRAINT [PK_MeasurementDevicesModels] PRIMARY KEY CLUSTERED ([ID] ASC)
-);
-
