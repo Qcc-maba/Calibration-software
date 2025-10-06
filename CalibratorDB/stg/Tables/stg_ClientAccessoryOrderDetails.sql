3ed55ae68592f5b2918f496182e6789e7207ee77
edtc <ed.kudlaev@gmail.com>
2025-10-06 17:51:12 +0300

    Add load ClientAccessoryOrderDetails

---
+++ CalibratorDB/stg/Tables/stg_ClientAccessoryOrderDetails.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [stg].[stg_ClientAccessoryOrderDetails] (
+    [SerialNumber]         NVARCHAR (30)  NULL,
+    [KLINE]                INT            NULL,
+    [ItemsCount]           INT            NULL,
+    [AccessoryDescription] NVARCHAR (200) NULL,
+    [SourceOrderId]        INT            NULL,
+    [AccessorySourceId]    INT            NULL,
+    [SourceSystem]         NVARCHAR (50)  NULL
+);
+
