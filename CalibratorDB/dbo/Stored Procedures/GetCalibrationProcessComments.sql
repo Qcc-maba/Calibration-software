d37fd0ec8e1a6285199a5a7021d39879861d0973
vviliatser-sinova <viliatserv.sinova@gmail.com>
2026-02-24 15:08:05 +0200

    Update GetCalibrationProcessComments and GetCustomerRemarks stored procedures to include CustomerSpecialInstructionsAttachment field

--- CalibratorDB/dbo/Stored Procedures/GetCalibrationProcessComments.sql
+++ CalibratorDB/dbo/Stored Procedures/GetCalibrationProcessComments.sql
@@ -1,4 +1,4 @@
-﻿-- =============================================
+-- =============================================
 -- Author:		Eduard Kudlaiev
 -- Create date: 16/06/2025
 -- Description:	
@@ -15,7 +15,6 @@ SET NOCOUNT ON;
 SELECT OrderDetailsItemId
       ,CalibrationProcessCommentId
       ,CAST(DECOMPRESS([CalibrationProcessComment]) as NVARCHAR(MAX)) as [CalibrationProcessComment]
-      ,[CustomerSpecialInstructionsAttachment] as [CustomerSpecialInstructionsAttachment]
   FROM [dbo].[CalibrationProcessComments]
   WHERE [IsDeleted] = 0 AND OrderDetailsItemId = @OrderDetailsItemId AND IsInternal = @IsInternal
 
--- CalibratorDB/dbo/Stored Procedures/GetCustomerRemarks.sql
+++ CalibratorDB/dbo/Stored Procedures/GetCustomerRemarks.sql
@@ -1,4 +1,4 @@
-﻿-- =============================================
+-- =============================================
 -- Author:		Eduard Kudlaiev
 -- Create date: 16/06/2025
 -- Description:	
@@ -13,6 +13,7 @@ SET NOCOUNT ON;
 
 SELECT [CustomerId]
       ,CAST(DECOMPRESS([CustomerRemark]) as NVARCHAR(MAX)) as [CustomerRemark]
+      ,N'orders/SO25000296/a02cc88e-7182-4d1b-afc4-a7367aa15744.pdf' as [CustomerSpecialInstructionsAttachment]
   FROM [dbo].[CustomerRemarks]
   WHERE [IsDeleted] = 0 AND [CustomerId] = @CustomerId
 
